41a77088daee837c0696b8cbf4accab1d5e5967a
Marcin Konieczny <marcin_k@coni.pl>
2017-09-27 10:17:46 +0200

    Version 1.0

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+
+        final String[] NAME = {"Sabrina","Kasandra","Roksana"}; // Zbieżność imion z czymkolwiek/kimkolwiek przypadkowa!
+
+        for(int temp = 0; temp < NAME.length; temp++){
+            System.out.println(NAME[temp]);
+        }
+
+    }
+}
